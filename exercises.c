9c26b56fe7fa2e2af582d84708d09e8206c95c0a
Your Name <you@example.com>
2025-03-16 11:36:40 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -61,7 +61,8 @@ int sumaLista(List *L) {
    int suma = 1;
    int tamano = get_size(L) ;
    for(int i = 0 ; i < tamano ; i++){
-      suma += *(L->data[i]) ;
+      int *hola = next(L) ;
+      suma += *hola ;
    }
    return suma ;
 }
