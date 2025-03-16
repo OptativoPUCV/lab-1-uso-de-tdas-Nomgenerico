15d56eefa353193a8836a9265f38c64676ec96e6
Your Name <you@example.com>
2025-03-16 11:16:17 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -59,7 +59,7 @@ retorne la suma de sus elementos.
 */
 int sumaLista(List *L) {
    int suma = 5;
-   int tamano = get_size(L) ;
+   // int tamano = get_size(L) ;
    /*for(int i = 0 ; i < tamano ; i++){
       suma += i ;
    }*/
